e893933b37b79b89fca817042c29592238f6c8ed
anupai267-pixel <anupai267@gmail.com>
2026-09-09 22:08:56 +0530

    parentheses

---
+++ dsa7.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main() {
+    string s = "({[]})";
+
+    stack<char> st;
+
+    for (char c : s) {
+        if (c == '(' || c == '{' || c == '[') {
+            st.push(c);
+        }
+        else {
+            if (st.empty()) {
+                cout << "false";
+                return 0;
+            }
+
+            if ((c == ')' && st.top() != '(') ||
+                (c == '}' && st.top() != '{') ||
+                (c == ']' && st.top() != '[')) {
+                cout << "false";
+                return 0;
+            }
+
+            st.pop();
+        }
+    }
+
+    if (st.empty())
+        cout << "true";
+    else
+        cout << "false";
+
+    return 0;
+}
